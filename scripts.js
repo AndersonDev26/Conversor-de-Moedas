09be396d97c2085eaa7f27e4387d4ebd8e1a5e90
AndersonDev26 <andsilv14@gmail.com>
2026-03-15 22:46:00 -0300

    cummit full

---
+++ scripts.js
@@ -0,0 +1,63 @@
+    const convertbutton = document.querySelector(".convert-button")
+    const currencyselect = document.querySelector(".currency-select")
+
+    function convertvalues(){
+        const inputcurrencyvalue = document.querySelector(".input-curency").value
+        const currencyvaluetoconvert = document.querySelector(".currency-value-to-convert")
+        const currencyvalueconverted = document.querySelector(".currency-value")
+
+        console.log(currencyselect.value)
+
+        const dolartoday = 5.2
+        const eurotoday = 6.2
+
+        
+
+        if (currencyselect.value == "dolar"){
+             currencyvalueconverted.innerHTML = new Intl.NumberFormat("en-us", {
+            style:"currency",
+            currency:"USD"
+        }).format(inputcurrencyvalue / dolartoday)
+        }
+
+        if (currencyselect.value == "euro"){
+            currencyvalueconverted.innerHTML = new Intl.NumberFormat("de-DE", {
+                style:"currency",
+                currency:"EUR"
+            }).format(inputcurrencyvalue/eurotoday)
+
+        }
+
+        currencyvaluetoconvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style:"currency",
+            currency:"BRL"
+        }).format(inputcurrencyvalue)
+
+       
+     
+        
+    }
+
+    function changecurrency (){
+        const currencyname = document.getElementById("currency-name")
+        const currencyimg = document.querySelector(".usa")
+
+
+        if(currencyselect.value == "dolar") {
+            currencyname.innerHTML = "Dólar"
+            currencyimg.src = "./assets/usa.png"
+        }
+
+
+         if(currencyselect.value == "euro") {
+            currencyname.innerHTML = "Euro"
+            currencyimg.src = "./assets/euro (3).png"
+        }
+
+        convertvalues()
+    }
+
+
+
+    currencyselect.addEventListener("change", changecurrency)
+    convertbutton.addEventListener("click", convertvalues )
